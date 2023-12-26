7fca2a182d18ed373545369bad266af708e5b032
ayushmalpani6 <145866524+ayushmalpani6@users.noreply.github.com>
2023-12-26 13:53:44 -0500

    init

---
+++ HiFolio/Authentication/View/LoginView.swift
@@ -0,0 +1,94 @@
+//
+//  LoginView.swift
+//  HiFolio
+//
+//  Created by Ayush Malpani on 12/17/23.
+//
+
+import SwiftUI
+
+struct LoginView: View  {
+    @State var emailInput: String = ""
+    @State var passwordInput: String = ""
+    @EnvironmentObject var viewModel: AuthViewModel
+    
+    var body: some View {
+        NavigationStack {
+            VStack{
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                    .padding(.vertical, 40)
+                
+                // form field
+                InputView(label: "Email Address",
+                          placeholder: "example@email.com",
+                          inputText: $emailInput)
+                .textInputAutocapitalization(.none)
+                .padding(.bottom, 20)
+                
+                InputView(label: "Password",
+                          placeholder: "Enter Password",
+                          inputText: $passwordInput,
+                          isSecureField: true)
+                .textInputAutocapitalization(.none)
+                
+                // forgot password button
+                Button {
+                    print("Trying to remember...")
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Forgot Password?")
+                            .foregroundStyle(.cyan)
+                            .fontWeight(.medium)
+                    }
+                    .padding(.top, 10)
+                    .padding(.horizontal, 20)
+                }
+                
+                // sign in button
+                
+                Button {
+                    Task {
+                        try await viewModel.signIn(withEmail: emailInput, password: passwordInput)
+                    }
+                } label: {
+                    HStack {
+                        Text("Sign In")
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 50)
+                            .foregroundColor(.white)
+                    }
+                    .background(.cyan)
+                    .clipShape(RoundedRectangle(cornerRadius: 10.0))
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                    .disabled(!formIsValid)
+                    .opacity(formIsValid ? 1.0 : 0.5)
+                }
+                
+                Spacer()
+                
+                // sign up button
+                NavigationLink {
+                    SignUpView()
+                } label: {
+                    Text("Don't have an account? **Sign Up**")
+                        .foregroundStyle(.cyan)
+                }
+                .navigationBarBackButtonHidden(true)
+            }
+        }
+    }
+}
+
+extension LoginView: AuthenticationFormProtocol {
+    var formIsValid: Bool {
+        return !emailInput.isEmpty && !passwordInput.isEmpty && emailInput.contains("@") && passwordInput.count > 5
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ HiFolio/Components/InputView.swift
@@ -0,0 +1,40 @@
+//
+//  InputView.swift
+//  HiFolio
+//
+//  Created by Ayush Malpani on 12/17/23.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var label: String
+    var placeholder: String
+    @Binding var inputText: String
+    var isSecureField: Bool = false
+    
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text(label)
+                .fontWeight(.semibold)
+                .foregroundStyle(.cyan)
+                
+            if isSecureField {
+                SecureField(label, text: $inputText, prompt: Text(placeholder))
+                    .fontWeight(.thin)
+            } else{
+                TextField(label, text: $inputText, prompt: Text(placeholder))
+                    .fontWeight(.thin)
+            }
+            Divider()
+        }
+        .padding(.horizontal, 30)
+    }
+}
+
+#Preview {
+    VStack {
+        InputView(label: "Email Address", placeholder: "example@fbla.com", inputText: .constant(""))
+        InputView(label: "Password", placeholder: "Enter Password", inputText: .constant(""), isSecureField: true)
+    }
+}
